d467d09cd5c0d88df74fe12bbc92cbeb3ae0cd5c
Florian Mirus <florian.mirus@gmx.net>
2016-09-26 20:57:23 +0200

    some minor fixes and modifications to show_retina to display and track two lasers

--- nstbot/examples/show_retina.py
+++ nstbot/examples/show_retina.py
@@ -1,13 +1,15 @@
 import nstbot
 
 import time
-bot = nstbot.RetinaBot()
-bot.connect(nstbot.Serial('/dev/ttyUSB0', baud=4000000))
-#bot.connect(connection.Socket('10.162.177.187'))
+bot = nstbot.PushBot()
+#bot.connect(nstbot.Serial('/dev/ttyUSB0', baud=4000000))
+bot.connect(nstbot.Socket('10.162.177.88'))
 #bot.connect(nstbot.Socket('192.168.1.161'))
 time.sleep(1)
 bot.retina(True)
 bot.show_image()
+bot.laser(100)
+bot.track_frequencies([100,100])
 #bot.track_spike_rate(
 #                     #all=(0,0,128,128),
 #                     left=(0,0,64,128),
